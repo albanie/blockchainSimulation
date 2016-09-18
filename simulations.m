b16c3de0d00ddc56c8e409bddda567398b6eca6f
albanie <albanie@robots.ox.ac.uk>
2016-09-18 13:19:59 +0100

    clean up comments

--- simulations.m
+++ simulations.m
@@ -1,4 +1,5 @@
-% some simple simulations in the style described in paper
+% This script contains some simple simulations in the style 
+% described in the bitcoin paper
 
 % consider a range of strengths for the attacker
 q = 0.1:0.05:0.45 ;
@@ -16,15 +17,21 @@ end
 
 % plot the results
 figure ;
+
 for i = 1:numel(q) 
     plot(z, probs(i,:)) ;
     hold on ;
 end
+
 ylim([0 1]) ;
 title('Attacker success odds') ;
 ylabel('Probability of attacker success') ;
 xlabel('Blocks waited by recipient') ;
 labels = arrayfun(@(x) sprintf('q = %.2f', x), q, 'UniformOutput', false) ;
 legend(labels) ;
+
+% save figure
 print('figs/simulations', '-dpng') ;
+
+% display in terminal
 zv_dispFig ;
