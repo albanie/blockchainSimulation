c281a7d862e4f972dec47565a29ab8711c61d347
albanie <albanie@robots.ox.ac.uk>
2016-09-18 13:16:06 +0100

    cleaned up startup file

--- startup.m
+++ startup.m
@@ -1,5 +1,5 @@
 % tool to display figures inline in terminal 
-run '~/coding/src/zvision/matlab/zv_setup' ;
+run('~/coding/src/zvision/matlab/zv_setup') ;
 
 % supress figure creation
 set(0,'DefaultFigureVisible','off') ;
